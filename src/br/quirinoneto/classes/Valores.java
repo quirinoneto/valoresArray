f275c982a0491816d31a96f84e9a0ac6a79ee39e
Antonio Quirino <quirinoneto@gmail.com>
2020-12-05 16:00:40 -0300

    Configurando o build usando o ant.

--- src/br/quirinoneto/classes/Valores.java
+++ src/br/quirinoneto/classes/Valores.java
@@ -10,19 +10,55 @@ public class Valores implements ValoresITF {
 	@Override
 	public boolean ins(int v) {
 		// TODO Auto-generated method stub
+		
+		for(int a = 0; a < array.length; a++) {
+			if (array[a] == 0) {
+				array[a] = v;
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
 	@Override
 	public int del(int i) {
 		// TODO Auto-generated method stub
-		return 0;
+		
+		int aux = 0;
+		boolean flag = false;
+		
+		for (int a = 0; a < array.length; a++) {
+			if (array[i] != 0) {
+				flag = true;
+			}
+		}
+				
+		if (flag) {
+			if (i >= 0 && i <= 9) {
+				aux = array[i];
+				array[i] = 0;
+			}
+			return aux;
+		} else {
+			return -1;
+		}
+		
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		
+		int cont = 0;
+		
+		for ( int i = 0; i < array.length; i++) {
+			if (array[i] != 0) {
+				cont++;
+			}
+		}
+		
+		return cont;
 	}
 	
 }
