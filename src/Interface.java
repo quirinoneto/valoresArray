0987a4547850f38790e7c3da48db7a0357502158
Antonio Quirino <quirinoneto@gmail.com>
2020-12-04 21:12:02 -0300

    Commit inicial.

---
+++ src/Interface.java
@@ -0,0 +1,8 @@
+
+public interface Interface {
+
+	boolean ins(int v); // insere um valor
+	int del (int i); // remove o valor armazenado no indíce i e retorna o valor removido
+	int size(); // retorna a quantidade de valores armazenados
+	
+}
